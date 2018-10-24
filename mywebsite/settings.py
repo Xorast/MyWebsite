1f313687dc3ff96683c720e6c30390b9d965a2f0
Xavier <xavier.astor.fontaine@gmail.com>
2018-10-24 16:52:33 +0000

    Allow new host

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['mysite-pangur.c9users.io','www.xavierastor.com']
+ALLOWED_HOSTS = ['mysite-pangur.c9users.io','xavierastor.herokuapp.com','www.xavierastor.com']
 
 
 # Application definition
